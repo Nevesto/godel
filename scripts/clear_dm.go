88a52452af3aa145f02b730e4c0278bdece5fec2
Nevesto <guilhermejcarlucci@hotmail.com>
2025-02-27 05:19:58 -0300

    fix rating limit

--- scripts/clear_dm.go
+++ scripts/clear_dm.go
@@ -1,12 +1,10 @@
 package scripts
 
 import (
-	"context"
 	"fmt"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
-	"golang.org/x/time/rate"
 )
 
 func ClearDM(session *discordgo.Session, userID string) error {
@@ -15,52 +13,37 @@ func ClearDM(session *discordgo.Session, userID string) error {
 		return fmt.Errorf("can't create dm: %w", err)
 	}
 
-	startTime := time.Now()
 	totalDeleted := 0
-	var myMessages []*discordgo.Message
-
 	var beforeID string
 
 	for {
-		messages, err := session.ChannelMessages(channel.ID, 100, beforeID, "", "")
+		messages, err := session.ChannelMessages(channel.ID, 30, beforeID, "", "")
 		if err != nil {
 			return fmt.Errorf("error when fetching messages: %w", err)
 		}
 
 		if len(messages) == 0 {
-			fmt.Println("All messages saved.")
+			fmt.Println("No more messages to delete.")
 			break
 		}
 
 		for _, message := range messages {
 			if message.Author.ID == session.State.User.ID {
-				myMessages = append(myMessages, message)
+				if err := session.ChannelMessageDelete(channel.ID, message.ID); err != nil {
+					return fmt.Errorf("can't delete message: %w", err)
+				}
+				fmt.Printf("Deleted message: %s\n", message.Content)
+				totalDeleted++
+
+				time.Sleep(1 * time.Second) // Avoid rate limiting
 			}
 		}
+
+		time.Sleep(15 * time.Second)
 
 		beforeID = messages[len(messages)-1].ID
 	}
 
-	limiter := rate.NewLimiter(rate.Every(250*time.Millisecond), 1)
-
-	ctx := context.Background()
-
-	for _, message := range myMessages {
-		err := limiter.Wait(ctx)
-		if err != nil {
-			return fmt.Errorf("rate limit error: %w", err)
-		}
-
-		err = session.ChannelMessageDelete(channel.ID, message.ID)
-		if err != nil {
-			return fmt.Errorf("can't delete message: %w", err)
-		} else {
-			fmt.Printf("message deleted: %s\n", message.Content)
-			totalDeleted++
-		}
-	}
-
-	elapsedTime := time.Since(startTime)
-	fmt.Printf("All messages deleted! Total: %d, Time: %s\n", totalDeleted, elapsedTime)
+	fmt.Printf("Deleted %d messages\n", totalDeleted)
 	return nil
 }
