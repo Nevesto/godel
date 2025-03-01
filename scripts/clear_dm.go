3bec0b59cc8ab9a26153a5459a80ade237c05832
nevest <guilhermejcarlucci@hotmail.com>
2025-03-01 01:35:35 -0300

    Last batch of messages verification

--- scripts/clear_dm.go
+++ scripts/clear_dm.go
@@ -23,7 +23,23 @@ func ClearDM(session *discordgo.Session, userID string) error {
 		}
 
 		if len(messages) == 0 {
-			fmt.Println("No more messages to delete.")
+			fmt.Println("No messages to delete")
+			break
+		}
+
+		if len(messages) < 30 { // Last batch of messages, don't need to fetch again
+			for _, message := range messages {
+				if message.Author.ID == session.State.User.ID {
+					if err := session.ChannelMessageDelete(channel.ID, message.ID); err != nil {
+						return fmt.Errorf("can't delete message: %w", err)
+					}
+					fmt.Printf("Deleted message: %s\n", message.Content)
+					totalDeleted++
+
+					time.Sleep(1 * time.Second) // Avoid rate limiting
+				}
+			}
+
 			break
 		}
 
@@ -39,6 +55,7 @@ func ClearDM(session *discordgo.Session, userID string) error {
 			}
 		}
 
+		fmt.Println("Waiting 15 seconds before fetching next batch of messages...")
 		time.Sleep(15 * time.Second)
 
 		beforeID = messages[len(messages)-1].ID
